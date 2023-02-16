bcc2ccb7ca471b117173f1b6692aa7ab5a6d907b
harris91 <flqlfn@gmail.com>
2023-02-16 17:20:30 +0900

    add(file): create vue project for practice 'SPA'

---
+++ 06_vue_spa/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <v-app-bar app color="secondray" dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>멀티 페이지</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <b-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </b-btn>
+    </v-app-bar>
+
+    <!-- 내용 영역에 라우터 페이지 렌더링 -->
+    <v-main>
+      <!-- 페이지 전환 효과 -->
+      <v-slide-x-trasition mode="out-in">
+        <router-view></router-view>
+      </v-slide-x-trasition>
+    </v-main>
+
+    <!-- footer 구성 -->
+    <v-footer color="secondary" foxed dark>
+      <div class="mx-auto">github.com/harris91</div>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ 06_vue_spa/src/components/main.vue
@@ -0,0 +1,21 @@
+<template lang="">
+    <v-container>
+        <p class="display-1 my-4">메인페이지</p>
+        <v-divider></v-divider>
+        <p class="display-4 my-4">메인페이지 입니다.</p>
+
+        <div class="text-center">
+            <v-btn fab large class="mt-5" color="purple" dark to="/sub">
+                <v-icon>mdi-arrow-right</v-icon>
+            </v-btn>
+        </div>
+    </v-container>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+
+</style>
